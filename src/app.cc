398824d20a0cadf100204555a40b70497a71a488
Fredrik Lind <fredrik.lind.96@gmail.com>
2021-12-28 22:25:15 +0100

    Implemented ImGUI

--- src/app.cc
+++ src/app.cc
@@ -2,24 +2,41 @@
 #include <GLFW/glfw3.h>
 #include <GL/gl3w.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <nanovg.h>
+#define NANOVG_GL3_IMPLEMENTATION
+#include <nanovg_gl.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define WINDOW_WIDTH 1366
 #define WINDOW_HEIGHT 768
 
+static void glfw_error_callback(int error, const char* msg)
+{
+	fprintf(stderr, "GLFW error %d: %s\n", error, msg);
+}
+
 int main(void)
 {
-	GLFWwindow* window;
+	glfwSetErrorCallback(glfw_error_callback);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	GLFWwindow* window;
 
 	if (!glfwInit())
 	{
-		fprintf(stderr, "Failed to initialize GLFW\n");
+		fprintf(stderr, "Failed to initialize GLFW!\n");
 		return EXIT_FAILURE;
 	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 	window = glfwCreateWindow(
 			WINDOW_WIDTH, WINDOW_HEIGHT, 
@@ -27,7 +44,7 @@ int main(void)
 
 	if (!window)
 	{
-		fprintf(stderr, "Failed to create window context\n");
+		fprintf(stderr, "Failed to create window context!\n");
 		glfwTerminate();
 		return EXIT_FAILURE;
 	}
@@ -40,6 +57,9 @@ int main(void)
 		return EXIT_FAILURE;
 	}
 
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_STENCIL_TEST);
+
 	if (!gl3wIsSupported(3, 2))
 	{
 		fprintf(stderr, "OpenGL 3.2 not supported on this hardware!\n");
@@ -50,11 +70,66 @@ int main(void)
 			glGetString(GL_VERSION),
 			glGetString(GL_SHADING_LANGUAGE_VERSION));
 
+	int fbWidth, fbHeight, winWidth, winHeight;
+	glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+	glfwGetWindowSize(window, &winWidth, &winHeight);
+	glfwSwapInterval(1);
+
+	float pxr = (float)fbWidth / (float)winWidth;
+
+	printf("Window = %dx%d, framebuffer = %dx%d, pxr = %f\n",
+			winWidth, winHeight, fbWidth, fbHeight, pxr);
+
+	NVGcontext* nc = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
+
+	IMGUI_CHECKVERSION();
+
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	bool show_demo_window = true;
+
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		glViewport(0, 0, fbWidth, fbHeight);
+
+		// Begin frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		nvgBeginFrame(nc, winWidth, winHeight, pxr);
+
+		if (show_demo_window)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+		// Render frame
+		ImGui::Render();
+
+		// End frame
+		nvgEndFrame(nc);
+
+		// Swap
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
 	}
 
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	nvgDeleteGL3(nc);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return EXIT_SUCCESS;
 }
